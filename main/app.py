1b5b5481169faa3753add7b0a0888186d2c7959a
Deni Kernjus <denikernjus@gmail.com>
2024-01-29 12:24:04 +0100

    final touch

--- main/app.py
+++ main/app.py
@@ -332,4 +332,4 @@ if __name__ == '__main__':
         #db.drop_all()
         db.create_all()
     app.secret_key = 'totallyuniqueSecretKey' 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4000)
